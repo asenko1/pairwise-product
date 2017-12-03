66cd9b0f7d36be2d76695bb8d963b7da0d3d9969
asenko1 <alex.senko598@gmail.com>
2017-12-03 12:00:53 -0800

    prep for stress test

--- src/mainFile.java
+++ src/mainFile.java
@@ -1,15 +1,27 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class mainFile {
 	public static void main(String[] args){
+		
+		System.out.println(pairwise_1(args[1]));
+		
+	}
+	
+	public static int pairwise_1(String array){
 		List<Integer> intList = new ArrayList<Integer>();
 		//args[1] is the int list
-		for(int i = 0; i < args[1].length(); i++){
-			intList.add((int) args[1].charAt(i) - '0');
+		for(int i = 0; i < array.length(); i++){
+			intList.add((int) array.charAt(i) - '0');
 		}
 		intList.sort(null);
-		System.out.println(intList.get(intList.size() - 1) * intList.get(intList.size() - 2));
+		return intList.get(intList.size() - 1) * intList.get(intList.size() - 2);
+	}
+	
+	public static void stressTest(){
+		
+		
 		
 	}
 
