dc90ed49e2bde44e59a7bce7ec512e7b3998e845
asenko1 <alex.senko598@gmail.com>
2017-12-04 16:19:33 -0800

    Basic random testing for exceptions

--- src/mainFile.java
+++ src/mainFile.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class mainFile {
 	public static void main(String[] args){
 		
 		System.out.println(pairwise_1(args[1]));
+		
+		stressTest(100, 0, 50);
+		
 		
 	}
 	
@@ -19,7 +23,21 @@ public class mainFile {
 		return intList.get(intList.size() - 1) * intList.get(intList.size() - 2);
 	}
 	
-	public static void stressTest(){
+	public static void stressTest(int num_tests, int min_val, int max_val){
+		//As is pretty much just checking for exceptions as is
+		String stringBuilder = "";
+		Random rand = new Random();
+		int[] testNums = new int[10];
+		for(int i = 0; i < num_tests; i++){
+			for(int j = 0; j < 10; j++){
+				testNums[j] = rand.nextInt(max_val) + min_val;
+			}
+			stringBuilder = "";
+			for(int j : testNums){
+				stringBuilder = stringBuilder + j; //could probably use StringBuffer
+			}
+			pairwise_1(stringBuilder);
+		}
 		
 		
 		
